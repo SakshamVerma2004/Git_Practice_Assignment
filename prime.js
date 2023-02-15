e37b05e229d72bad8288c52bd333d59ef3001261
SakshamVerma2004 <reemaverma001@gmail.com>
2023-02-15 14:27:05 +0530

    added prime file

--- prime.js
+++ prime.js
@@ -1,12 +1,13 @@
-function Prime(num){
-
+let num=10;
 let count=0;
 for(let i=1;i<=num;i++){
    if(num%i==0){
       count++;
    }
 }
-if(count==2){
-   console.log("Prime");
+if(count==0{
+  console.log("True");
 }
+else{
+console.log("False");
 }
